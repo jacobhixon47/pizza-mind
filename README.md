5d69d5feab5848c8907e9d7e3c0e077526daccad
Jacob Hixon <jacobhixon47@gmail.com>
2016-11-04 16:58:42 -0700

    Add logic to return selections, add confirmation page, README

---
+++ README.md
@@ -0,0 +1,37 @@
+# _PIZZAMIND_
+
+#### _Pizza order website, 11/4/16_
+
+#### By _**Jacob Hixon**_
+
+## Description
+
+_Order a pizza and recieve an order summary with price, size, and toppings._
+
+## Specifications
+
+1. _Returns size_
+  * _Example select: Large_
+  * _Expected output: Size: Large_
+
+2. _Returns the toppings_
+  * _Example select: Pepperoni_
+  * _Expected output: Toppings: Pepperoni_
+
+3. _Returns price_
+  * _Example select: small($7.00), pepperoni($1.00)_
+  * _Expected output: Price: $8.00_
+
+## Technologies Used
+
+* _HTML_
+* _CSS_
+* _Bootstrap_
+* _Javascript_
+* _jQuery_
+
+### License
+
+*MIT*
+
+Copyright (c) 2016 **_Jacob Hixon_**
--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,76 @@
-function Pizza(size) {
+// Business Logic
+
+function Pizza(size, price) {
   this.toppings = [];
   this.size = size;
+  this.price = price;
 };
 
+Pizza.prototype.cost = function() {
+  return "$" + this.price;
+};
 
-
+// User Interface Logic //
 $(document).ready(function() {
   $("#nav-list").fadeIn(1500);
   $(".row").fadeIn(1500);
-  
+  $(".order-button").click(function(event) {
+    event.preventDefault();
+
+    $(".row").slideUp(500, function() {
+      $("#order-row").slideDown();
+    });
+  });
+
+  $(".quench-button").click(function() {
+    $('html, body').animate( {
+      scrollTop: ($('#row-2').offset().top)
+    },1000);
+  });
+
+  $(".wings-button").click(function() {
+    $('html, body').animate( {
+      scrollTop: ($('#row-3').offset().top)
+    },1000);
+  });
+
+  $("#order-form").submit(function(event) {
+    event.preventDefault();
+
+    pizzaCost = 0;
+    $(".wings-div").empty();
+    var newPizza = new Pizza("", 0);
+
+    if ($("option[value='small']").is(":selected") === true) {
+      newPizza.size = "Small";
+      pizzaCost += 7;
+    }
+    else if ($("option[value='medium']").is(":selected") === true) {
+      newPizza.size = "Medium";
+      pizzaCost += 14;
+    }
+    else if ($("option[value='large']").is(":selected") === true) {
+      newPizza.size = "Large";
+      pizzaCost += 21;
+    }
+    if ($("input[value='Wings']").is(":checked") === true) {
+      $(".wings-div").append("<h2>" + "Wings:" + "</h2><br><h4>" + "10pc Boneless Wings" + "</h4>")
+    }
+    $("input[name='1']:checked").each(function() {
+      newPizza.toppings.push($(this).val());
+      pizzaCost += 1;
+    });
+    newPizza.price = pizzaCost;
+    $("span#size").empty();
+    $("span#size").append(newPizza.size);
+    $("span#toppings").empty();
+    newPizza.toppings.forEach(function(topping) {
+      $("span#toppings").append(topping + "<br>")
+    });
+    $("span#total").empty();
+    $("span#total").append(newPizza.cost());
+
+    $("#order-row").slideUp();
+    $("#confirmation-row").slideDown();
+  });
 });
